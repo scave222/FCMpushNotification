063330d99468a4bd2d52b5e451439f45d5504420
scave222 <50265878+scave222@users.noreply.github.com>
2021-10-25 17:36:11 +0100

    Add project files.

---
+++ FCMpushNotification/Program.cs
@@ -0,0 +1,45 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+
+namespace FCMpushNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("private_key.json")
+            });
+
+            //This registartion token comes from the client FCM SDKs.
+            var registrationToken = "Add_TOKEN";
+
+            // message payload
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "myData", "1337" },
+                },
+
+                //Token = registrationToken,
+
+                Topic = "all",
+                Notification = new Notification()
+                {
+                    Title = "Test from code",
+                    Body = "Here is your test!"
+                }
+            };
+
+            //Send a message to the device corresponding to the provided registartion token
+            string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+            //Response is a message ID string.
+            Console.WriteLine("Successfully sent message:" + response);
+        }
+    }
+}
